f325e1a750fd6188b5a3daed0e3cec86b9f1c5a7
ulises <ulipalazzoo@gmail.com>
2021-09-07 12:44:31 -0300

    modifico archivos

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,23 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_consultar"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        consulta = next(tracker.get_latest_entity_values("consulta"), None)
+        if str(consulta)=="precio":
+            message="Estos son los precios de nuestros productos: Sopas: $40, Fideos $30, Papel HIgienico $1500"
+
+        dispatcher.utter_message(text=str(message))
+
+        return []
